689b4cecfb0d2229a60a442b61ff9f085ecc4e9b
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-30 15:21:06 -0300

    Adition of path in function

--- Projeto 2/random.c
+++ Projeto 2/random.c
@@ -3,21 +3,22 @@
 #include <time.h>
 #include <string.h>
 
-void get_random_file(char * files_names);
+void get_random_file(char * file_name, char * file_path);
 //Função gera 25 nomes de arquivo aleatório entre os apresentados
 int main()
 {
 	srand(time(0));	// Usado para rand() gerar número aleátorio cada vez que rodado
 
 	char files_names[25][50];
+	char files_path[25][50];
 	for(int i=0; i<25; i++){
-		get_random_file(files_names[i]);
-		printf("%s\n", files_names[i]);
+		get_random_file(files_names[i], files_path[i]);
+		printf("%s\n", files_path[i]);
 	}
 	return 0;
 
 }
-void get_random_file(char * file_name){
+void get_random_file(char * file_name, char * file_path){
 	
 	
 	char data_type[50]; 
@@ -31,6 +32,7 @@ void get_random_file(char * file_name){
 	}
 	int random_50 = (rand()%50)+1; //Gera número aleatório de 1 a 50 
 	sprintf(file_name ,"%s_%02d.txt", data_type, random_50);
+	sprintf(file_path ,"./DataSet/%s/%s", data_type, file_name);
 	
 }
 
