fd44469667bf97ff53deafd4dca81e50a687187a
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-30 16:29:33 -0300

    Finalização da leitura das imagens feita aleatoriamente
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- Projeto 2/random.c
+++ Projeto 2/random.c
@@ -3,22 +3,59 @@
 #include <time.h>
 #include <string.h>
 
-void get_random_file(char * file_name, char * file_path);
+void get_random_file( char * file_path);
 //Função gera 25 nomes de arquivo aleatório entre os apresentados
 int main()
 {
 	srand(time(0));	// Usado para rand() gerar número aleátorio cada vez que rodado
 
-	char files_names[25][50];
-	char files_path[25][50];
+	//Geração aleatória dos 25 nomes de arquivos
+	char file_path[25][50];
 	for(int i=0; i<25; i++){
-		get_random_file(files_names[i], files_path[i]);
-		printf("%s\n", files_path[i]);
+		get_random_file(file_path[i]);
+		//printf("%s\n", file_path[i]);
+	}
+
+	int *imagens[25]; // Vetor com informaçoes das 25 imagens
+	//Leitura dos 25 arquivos gerados aleatoriamente	
+	for(int i=0; i<25; i++){
+		FILE *arquivo;
+		arquivo = fopen(file_path[i], "r");
+
+		if (arquivo == NULL){
+			printf("Problema na Leitura do arquivo\n");
+			exit(1);
+		}
+		
+		imagens[i] = (int *) malloc (sizeof(int));
+		long int j=0;
+		while (!feof(arquivo))
+		{	
+			fscanf(arquivo, "%d;", &imagens[i][j]);
+			//printf("imagens[%d]= %d\n", i, imagens[i][j]);		
+			imagens[i] = (int *) realloc(imagens[i], (j+2)*sizeof(int));
+
+			if (imagens[i] == NULL){
+				printf("Alocacao falhou. Finalizado.\n");
+				exit(1);
+			}
+			j++;
+			
+		}
+		fclose(arquivo);
+	}
+
+
+
+
+	//Liberação da memória contendo arquivos de imagens
+	for(int i=0; i<25; i++){
+		free(imagens[i]);
 	}
 	return 0;
 
 }
-void get_random_file(char * file_name, char * file_path){
+void get_random_file(char * file_path){
 	
 	
 	char data_type[50]; 
@@ -31,6 +68,7 @@ void get_random_file(char * file_name, char * file_path){
 		strcpy(data_type, "grass");
 	}
 	int random_50 = (rand()%50)+1; //Gera número aleatório de 1 a 50 
+	char file_name[50];
 	sprintf(file_name ,"%s_%02d.txt", data_type, random_50);
 	sprintf(file_path ,"./DataSet/%s/%s", data_type, file_name);
 	
