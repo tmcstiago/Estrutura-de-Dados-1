171f888341cb34b6cd81ebda68c6a60590527aa8
Tiago Miguel <tmcstiago@gmail.com>
2018-10-16 23:20:38 -0300

    definindo a struct

---
+++ Projeto 3/projeto3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Pessoa {
+	char nome[101];
+	char telefone[11];
+	char endereco[101];
+	int cep;
+	char telefone[11];
+	contato *proximo;
+	contato *anterior;
+};	
+
+typedef struct Pessoa contato;
+
+
+int main () {
+
+
+
+
+
+
+
+	return 0;
+}
