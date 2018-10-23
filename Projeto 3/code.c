c3d2c3673280ab1a2f67a367c687fda3c47bcc2c
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-10-23 13:03:54 -0300

    Ajusts in busca_contato function

--- Projeto 3/code.c
+++ Projeto 3/code.c
@@ -16,24 +16,27 @@ typedef struct Pessoa contato;
 
 contato *lista_contatos_vazia();
 contato *leitura_inicial();
+void salvar_arquivo(contato *contatos); 
+contato * insertion_sort(contato *contatos); 
+void libera_contatos(contato *contatos); 
 contato *novo_contato(contato *contatos, contato *novo);
-void libera_contatos(contato *contatos);
 void imprimi_contatos(contato *contatos);
-void salvar_arquivo(contato *contatos);
-void copy(contato* A, contato* B);
-contato * insertion_sort(contato *contatos);
+void busca_contato(contato *contatos, char *busca);
+void remove_contato(contato *contatos, char *busca);
+int verificador_telefone(contato *novo, char *t);
+int verificador_nascimento(contato *novo, char *t);
 
 int main () {
+
 	contato *contatos;
 	contato *novo;
-	contatos = lista_contatos_vazia();
 	contatos = leitura_inicial();
-	contatos = insertion_sort(contatos);
-	//imprimi_contatos(contatos);
+	char busca[101];
+	char *ponto;
+	printf("********** Seja bem-vindo a sua lista de contatos **********\n\n");
+	
 	int opcao;
 	do{
-		system("clear");
-		printf("********** Seja bem-vindo a sua lista de contatos **********\n\n");
 		printf("1. Inserir novo contato\n");
 		printf("2. Remover contatos\n");
 		printf("3. Realizar busca de usuários\n");
@@ -41,10 +44,7 @@ int main () {
 		printf("5. Fechar a lista de contatos\n");
 		printf("Indique o número de uma das opções acima: ");
 		scanf ("%d", &opcao);
-		getchar();
-		char lixo;
-		//scanf("%c", &lixo);
-		system("clear");
+
 		switch (opcao){
 			case 1:
 				novo = lista_contatos_vazia();
@@ -56,57 +56,85 @@ int main () {
 				printf("Insira o nome do contato: ");
 				scanf(" %[^\n]", novo->nome);
 
-				char t[11];
-				printf("Insira o Telefone: ");
-				scanf(" %[^\n]", t);
-
 				//verificador do telefone
-				int i;
-				int j;
-				for(i=0, j=0; i<10; i++, j++) {
-					if (j==5){
-						if(t[i] == '-'){
-							novo->telefone[j] = '-';
-							j++;
-						}
-					}
-					novo->telefone[i] = t[j];
+				while (1){
+					char t[11];
+					printf("Insira o Telefone: ");
+					scanf(" %[^\n]", t);
+					ponto = t;
+
+					int certo = verificador_telefone(novo, ponto);
+
+					if(certo)
+						break;
+
+					printf("\n*** Telefone inválido, utilize o formato xxxxx-xxxx ***\n\n");
+
 				}
-			
 
 				printf("Insira o endereço: ");
 				scanf(" %[^\n]", novo->endereco);
 
-				printf("Insira o CEP (padrão EUA): ");
-				scanf(" %u", &novo->cep);
 
-				printf("Insira a data de nascimento: ");
-				scanf(" %[^\n]", novo->nascimento);
+				//verificador do CEP
+				while (1){
+ 					printf("Insira o CEP (padrão EUA): ");
+					scanf(" %u", &novo->cep);
+					
+					if(novo->cep > 9999 && novo->cep < 100000)
+						break;
 
-				printf("Nome: %s\n", novo->nome);
-				printf("Telefone: %s\n", novo->telefone);
-				printf("Endereço: %s\n", novo->endereco);
-				printf("CEP: %u\n", novo->cep);
-				printf("Data de Nascimento: %s\n", novo->nascimento);
-				getchar();
+					printf("\n*** CEP invalido, utilize o formato xxxxx ***\n\n");
+				}
+
+
+				//verificador data de nascimento
+				while (1){
+					char t[11];
+
+					printf("Insira a data de nascimento: ");
+					scanf(" %[^\n]", t);
+
+					ponto = t;
+
+					int certo = verificador_nascimento(novo, ponto);
+
+					if(certo)
+						break;
+
+					printf("\n*** Data inválida, utilize o formato dd/mm/aaaa ***\n\n");
+				}
+
+				// printf("Nome: %s\n", novo->nome);
+				// printf("Telefone: %s\n", novo->telefone);
+				// printf("Endereço: %s\n", novo->endereco);
+				// printf("CEP: %u\n", novo->cep);
+				// printf("Data de Nascimento: %s\n", novo->nascimento);
+
 				contatos = novo_contato(contatos, novo);
-				insertion_sort(contatos);
-				printf("Cadastro realizado com sucesso!\n");
-				printf("Aperte enter para continuar:");
-				getchar();
-				//free (novo);
+
+				printf("\n\n");
+				// free (novo);
 			break;
 			case 2:
+				printf("Insira o nome do cotato(s) que você deseja remover: ");
+				scanf(" %[^\n]", busca);
 
+				ponto = busca;
+
+				remove_contato(contatos, busca);
 			break;
 			case 3:
+				printf("Insira o nome do cotato(s) que você deseja buscar: ");
+				scanf(" %[^\n]", busca);
 
+				ponto = busca;
+
+				busca_contato(contatos, busca);
 			break;
 			case 4:
 				contatos = insertion_sort(contatos);
 				imprimi_contatos(contatos);
-				printf("Aperte enter para continuar:");
-				getchar();
 
 			break;
 			case 5:
@@ -114,10 +142,9 @@ int main () {
 		}
 
 	} while (opcao != 5);
-	insertion_sort(contatos);
+	contatos = insertion_sort(contatos);
 	salvar_arquivo(contatos);
 	libera_contatos(contatos);
-
 
 	return 0;
 }
@@ -126,6 +153,8 @@ int main () {
 contato *lista_contatos_vazia(){
 	return NULL;
 }
+
+
 void libera_contatos(contato *contatos){
 	contato *atual;
 	for (atual = contatos; atual != NULL; contatos = atual){
@@ -189,34 +218,6 @@ contato * insertion_sort(contato *contatos){
 		//getchar();
 	}
 }
-void copy(contato* A, contato* B){
-	contato* temp;
-	temp = (contato *) malloc(sizeof(contato));
-	if(temp==NULL) exit(-1);
-
-	//strcpy(temp->nome,A->nome);
-	strcpy(A->nome,B->nome);
-	//strcpy(B->nome,temp->nome);
-	
-	//strcpy(temp->telefone,A->telefone);
-	strcpy(A->telefone,B->telefone);
-	//strcpy(B->telefone,temp->telefone);
-
-	//strcpy(temp->endereco,A->endereco);
-	strcpy(A->endereco,B->endereco);
-	//strcpy(B->endereco,temp->endereco);
-
-	//temp->cep=A->cep;
-	A->cep=B->cep;
-	//B->cep=temp->cep;
-
-	//strcpy(temp->nascimento,A->nascimento);
-	strcpy(A->nascimento,B->nascimento);
-	A->anterior=B->anterior;
-	A->proximo=B->proximo;
-	//strcpy(B->nascimento,temp->nascimento);
-	free(temp);
-}	
 void salvar_arquivo(contato *contatos){
 	FILE *arquivo;
 	arquivo = fopen("contatos.txt", "w");
@@ -289,10 +290,9 @@ contato * leitura_inicial(){
 	return contatos;
 }
 
+
 contato *novo_contato(contato *contatos, contato *novo) {
 	if (contatos == NULL){
-		contatos = (contato *) malloc(sizeof(contato));
-		if(contatos==NULL) exit(-1);
 		novo->anterior = NULL;
 		novo->proximo = NULL;
 
@@ -301,28 +301,215 @@ contato *novo_contato(contato *contatos, contato *novo) {
 	else {
 		contato *atual;
 
-		for (atual = contatos; atual->proximo != NULL; atual = atual->proximo);
+		for (atual = contatos; atual->proximo != NULL; atual = atual->proximo){}
 		atual->proximo = novo;
 		novo->anterior = contatos;
 		novo->proximo = NULL;
-		//printf("Nome: %s\n", contatos->nome);
 		return contatos;
 	}
 }
 
+
 void imprimi_contatos(contato *contatos){
 	contato *atual;
-	int i=1;
-	for (atual = contatos; atual != NULL; atual = atual->proximo){
-		printf("Nome: %s\n", atual->nome);
-		printf("Contato %d\n", i);
-		printf("Telefone: %s\n", atual->telefone);
-		printf("Endereço: %s\n", atual->endereco);
-		printf("CEP: %u\n", atual->cep);
-		printf("Data de Nascimento: %s\n", atual->nascimento);
-		printf("\n\n\n");
-		i++;
-	}
 
+	if (contatos != NULL){
+		int i=1;
+		for (atual = contatos; atual != NULL; atual = atual->proximo){
+			printf("Nome: %s\n", atual->nome);
+			printf("Telefone: %s\n", atual->telefone);
+			printf("Endereço: %s\n", atual->endereco);
+			printf("CEP: %u\n", atual->cep);
+			printf("Data de Nascimento: %s\n", atual->nascimento);
+			printf("\n");
+			i++;
+		}
+	}
+	else {
+		printf("Agenda Vazia!!\n" );
+	}
+	
+	printf("\n\n\n");
 }
 
+
+void busca_contato(contato *contatos, char *busca){
+	contato *atual;
+
+	int cont_loc = 0;
+	for(atual = contatos; atual != NULL; atual = atual->proximo){
+		int j = 0;
+		for(int i=0; ; i++){
+			if (*(busca+j)>= 'a' && *(busca+j)<='z'){
+				*(busca+j) = *(busca+j)-32;
+			}
+			if (*(busca+j) == '\0'){
+				if (cont_loc==0){
+					printf("\nContato(s) localizados:\n\n");
+				}
+				printf("Nome: %s\n", atual->nome);
+				printf("Telefone: %s\n", atual->telefone);
+				printf("Endereço: %s\n", atual->endereco);
+				printf("CEP: %u\n", atual->cep);
+				printf("Data de Nascimento: %s\n", atual->nascimento);
+				printf("\n");
+				cont_loc++;
+				break;
+			}
+			else {
+				if(atual->nome[i] == *(busca+j) || atual->nome[i] == *(busca+j)+32){
+					j++;
+				}
+				else {
+					j=0;
+				}
+			}
+			if( atual->nome[i] == '\0')
+				break;
+		}
+	}
+	if (cont_loc==0){
+		printf("\nNenhum contato localizado\n");
+	}
+}
+
+
+void remove_contato(contato *contatos, char *busca){
+	busca_contato(contatos, busca);
+
+	char confirmar;
+	printf("Deseja excluir os contatos acima? [s/n]: ");
+	scanf(" %c", &confirmar);
+
+	if (confirmar == 's' ||  confirmar == 'S') {
+		contato *atual;
+
+		atual = contatos;
+		do{
+			int j=0;
+			for(int i=0; atual->nome[i] != '\0'; i++){
+				if (*(busca+j)>= 97 && *(busca+j)<=122){
+					*(busca+j) = *(busca+j)-32;
+				}
+				if (*(busca+j) == '\0'){
+					contato *excluir;
+
+					if (atual->anterior==NULL && atual->proximo == NULL){
+						free(contatos);
+
+						contatos = lista_contatos_vazia();
+
+						goto finalizado;
+					}
+					else if (atual->anterior==NULL){
+						contato *p;
+
+						p = atual->proximo;
+						p->anterior = NULL;
+
+						excluir = atual;
+						atual = atual->proximo;
+					}
+					else if(atual->proximo == NULL){
+						contato *a;
+
+						a = atual->anterior;
+						a->proximo = NULL;
+
+						excluir = atual;
+						atual = atual->proximo;
+					}
+					else{
+						contato *a;
+						contato *p;
+
+						a = atual->anterior;
+						a->proximo = atual->proximo;
+
+						p = atual->proximo;
+						p->anterior = atual->anterior;
+
+						excluir = atual;
+						atual = atual->proximo;
+					}
+					j = 555;
+					free(excluir);
+					break;
+				}
+				else {
+					if(atual->nome[i] == *(busca+j) || atual->nome[i] == *(busca+j)+32){
+						j++;
+					}
+					else {
+						j=0;
+					}
+				}
+			}
+			if (j != 555){
+				atual = atual->proximo;
+			}
+		} while (atual != NULL);
+		finalizado: printf("\nVocê não tem mais nennhum contato na sua agenda\n\n");
+	}
+}
+
+
+int verificador_telefone(contato *novo, char *t) {
+	int i;
+	int j;
+	for(i=0, j=0; i<10; i++, j++) {
+		if(i != 5){
+			if(*(t+j) >= 48 && *(t+j) <= 57);
+			else
+				return 0;
+		}
+		else{
+			if(*(t+i) == '-');
+			else if(*(t+j) >= 48 && *(t+j) <= 57){
+				novo->telefone[i] = '-';
+				i++;
+			}
+			else
+				return 0;
+		}
+		novo->telefone[i] = *(t+j);
+	}
+	return 1;
+}
+
+
+int verificador_nascimento(contato *novo, char *t) {
+	int i;
+	int j;
+	int cont;
+	for(i=0, j=0, cont=0; i<10; i++, j++) {
+		if(i == 2){
+			if(*(t+j) == '/')
+				cont++;
+			else if(*(t+j) >= 48 && *(t+j) <= 57){
+				novo->nascimento[i] = '/';
+				i++;
+			}
+			else
+				return 0;
+		}
+		else if(i == 5){
+			if(*(t+j) == '/' && cont==1);
+			else if (*(t+j) != '/' && cont==1)
+				return 0;
+			else if(*(t+j) >= 48 && *(t+j) <= 57){
+				novo->nascimento[i] = '/';
+				i++;
+			}
+			else
+				return 0;
+		}
+		else{
+			if(*(t+j) >= 48 && *(t+j) <= 57);
+			else
+				return 0;
+		}
+		novo->nascimento[i] = *(t+j);
+	}
+	return 1;
+}
