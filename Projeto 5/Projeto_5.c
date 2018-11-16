7ee7ab962c48dfce94c1cedfa8f3b43be4b2a550
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-11-16 14:53:30 -0200

    Adition of iniciar_arvore function

--- Projeto 5/Projeto_5.c
+++ Projeto 5/Projeto_5.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+typedef struct ArvoreBinaria{
+	int valor;
+	struct ArvoreBinaria * esquerda;
+	struct ArvoreBinaria * direita;	
+} arvore;
 
+arvore * iniciar_arvore();
+
+int main(){
+	arvore *numeros; 
+	numeros = iniciar_arvore();
+	printf("endereço da arvore %p\n", numeros);
 
 	return 0;
 }
+arvore * iniciar_arvore(){
+	arvore *nova_arvore;
+	nova_arvore = (arvore *) malloc(sizeof(arvore));
+	if(nova_arvore==NULL){
+		printf("Alocação falhou\n");
+		exit(-1);
+	}
+	nova_arvore->esquerda = NULL;
+	nova_arvore->direita = NULL;
+	return nova_arvore;
+}
+
